4326fc4631b96c02efdd2f48df9683da0e00725b
rustemvakhitov <rustemvakhitov@hotmail.com>
2025-02-03 10:59:35 -0700

    added just different ways to past params to web method

--- WebAppDocker/WebHost.py
+++ WebAppDocker/WebHost.py
@@ -6,19 +6,26 @@ app = Flask(__name__)
 def handle_404(error):
     return error
 
-@app.route('/my_get', methods=['GET'])
-def handle_get():
-    if request.method == 'GET':
-        param1 = request.args['param1']
-        param2 = request.args['param2']
+#get params as URL parameters
+@app.route('/my_getP', methods=['GET'])
+def handle_getP():
+    param1 = request.args['param1']
+    param2 = request.args['param2']
 
 
     #let's just return parameters back to a client as JSON
     return jsonify(param1=param1, param2=param2)
-        
 
-@app.route('/my_post', methods=['POST'])
-def handle_post():
+#get params as part of URL
+@app.route('/my_getU/<param1>/<param2>', methods=['GET'])
+def handle_getU(param1, param2):
+
+    #let's just return parameters back to a client as JSON
+    return jsonify(param1=param1, param2=param2)
+
+#get params as part as body
+@app.route('/my_getB', methods=['GET'])
+def handle_getB():
     if request.data == None :
         raise Exception("data in request is null","WebHost")
     
@@ -27,7 +34,39 @@ def handle_post():
     # do something with data you got from client
 
     #when done return success
-    return jsonify(success=True)
+    return jsonify(success=True, data=str(data))      
+
+#POST
+
+#get params as URL parameters
+@app.route('/my_postP', methods=['POST'])
+def handle_postP():
+    param1 = request.args['param1']
+    param2 = request.args['param2']
+
+
+    #let's just return parameters back to a client as JSON
+    return jsonify(param1=param1, param2=param2)
+
+#get params as part of URL
+@app.route('/my_postU/<param1>/<param2>', methods=['POST'])
+def handle_postU(param1, param2):
+
+    #let's just return parameters back to a client as JSON
+    return jsonify(param1=param1, param2=param2)
+
+#get params as part as body
+@app.route('/my_postB', methods=['POST'])
+def handle_postB():
+    if request.data == None :
+        raise Exception("data in request is null","WebHost")
+    
+    data = request.data
+    
+    # do something with data you got from client
+
+    #when done return success
+    return jsonify(success=True, data=str(data))
 
 
 if __name__ == '__main__':
